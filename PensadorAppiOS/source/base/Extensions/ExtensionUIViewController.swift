4e30ec5a79959765c5bdec11b3d2cd7e87201a3e
gabriel.camargo <gabriel.camargo@iteris.com.br>
2019-03-26 16:16:45 -0300

    adjust toast

--- PensadorAppiOS/source/base/Extensions/ExtensionUIViewController.swift
+++ PensadorAppiOS/source/base/Extensions/ExtensionUIViewController.swift
@@ -12,18 +12,22 @@ extension UIViewController {
     
     func showToast(message : String, color: UIColor) {
         let toastLabel = UILabel(frame: CGRect(x: 0, y: self.view.frame.size.height-100, width:self.view.frame.size.width , height: self.view.frame.size.height - (self.view.frame.size.height-100)))
-        toastLabel.backgroundColor = color.withAlphaComponent(0.6)
+        toastLabel.backgroundColor = color
         toastLabel.textColor = UIColor.white
         toastLabel.textAlignment = .center;
-        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.font = UIFont(name: "Montserrat-Bold", size: 12.0)
         toastLabel.text = message
         toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 5
-        toastLabel.clipsToBounds  =  true
+//        toastLabel.layer.cornerRadius = 5
+//        toastLabel.clipsToBounds  =  true
         self.view.addSubview(toastLabel)
-        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
-        }, completion: {(isCompleted) in
-            toastLabel.removeFromSuperview()
-        })
-    } }
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+            UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseOut, animations: {
+                toastLabel.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastLabel.removeFromSuperview()
+            })
+        }
+    }
+    
+}
